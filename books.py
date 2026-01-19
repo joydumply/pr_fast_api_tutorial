395f0f82c5b28c09161ad2d58d92afabba991dec
joydumply <nick.babichev@gmail.com>
2026-01-20 00:17:41 +0400

    Adds endpoints for querying and creating books
    Introduces query parameter endpoints to filter books by category and by author with category, enhancing search flexibility.
    Implements book creation endpoint to allow adding new entries.
    Improves code clarity and consistency in formatting.

--- books.py
+++ books.py
@@ -1,22 +1,57 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
-app = FastAPI() # creating a new app FastAPI
+app = FastAPI()  # creating a new app FastAPI
 
 BOOKS = [
-	{'title': 'Title One', 'author': 'Author One', 'category': 'science'},
-	{'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
-	{'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
-	{'title': 'Title Four', 'author': 'Author Four', 'category': 'math'},
-	{'title': 'Naruto', 'author': 'Massasi Kishimoto', 'category': 'legend'},
+    {"title": "Title One", "author": "Author One", "category": "science"},
+    {"title": "Title Two", "author": "Author Two", "category": "science"},
+    {"title": "Title Three", "author": "Author Three", "category": "history"},
+    {"title": "Title Four", "author": "Author Two", "category": "math"},
+    {"title": "Title Four And One", "author": "Author Two", "category": "math"},
+    {"title": "Naruto", "author": "Massasi Kishimoto", "category": "legend"},
 ]
+
 
 @app.get("/books")
 async def read_all_books():
-	return BOOKS
+    return BOOKS
+
 
 # Dynamic parameter
 @app.get("/books/{book_title}")
 async def read_book(book_title: str):
-	for book in BOOKS:
-		if book.get('title').casefold() == book_title.casefold(): #casefold() - is lowercase() analogue
-			return book
+    for book in BOOKS:
+        if (
+            book.get("title").casefold() == book_title.casefold()
+        ):  # casefold() - is lowercase() analogue
+            return book
+
+
+# Query params
+@app.get("/books/")
+async def read_category_by_query(category: str):
+    books = []
+    for book in BOOKS:
+        if book.get("category").casefold() == category.casefold():
+            books.append(book)
+
+    return books
+
+
+@app.get("/books/{book_author}/")
+async def read_author_category_by_query(book_author: str, cat: str):
+    books_return = []
+
+    for book in BOOKS:
+        if (
+            book.get("author").casefold() == book_author.casefold()
+            and book.get("category").casefold() == cat.casefold()
+        ):
+            books_return.append(book)
+
+    return books_return
+
+
+@app.post("/books/create_book")
+async def create_book(new_book=Body()):
+    BOOKS.append(new_book)
