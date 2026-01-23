e5d03e38a6884d0759d74fb8a5836f9ce273bca9
joydumply <nick.babichev@gmail.com>
2026-01-23 13:26:54 +0400

    implemented PUT method

--- books2.py
+++ books2.py
@@ -54,3 +54,9 @@ def find_book_id(book: Book):
     book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1  # Тернарный оператор
 
     return book
+
+@app.put("/books/update_book")
+def update_book(book:BookRequest):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book.id:
+            BOOKS[i] = book
